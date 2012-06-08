cb681de335f4f358699d60ef979bfebb9c985984
Jacob Hesch <jacob@nextdoor.com>
2012-06-08 16:19:20 -0700

    Add copyright headers and setup.py metadata.

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+# Copyright 2012 Nextdoor.com, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import shutil
 import subprocess
@@ -39,6 +53,7 @@ setup(
     author='Jacob Hesch',
     author_email='jacob+git-change@nextdoor.com',
     url='https://github.com/Nextdoor/git-change',
+    license='Apache License, Version 2.0',
     keywords='gerrit git code review',
     packages=[PACKAGE],
     entry_points={
@@ -54,6 +69,7 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Topic :: Software Development',
+        'License :: OSI Approved :: Apache Software License',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Operating System :: POSIX',
